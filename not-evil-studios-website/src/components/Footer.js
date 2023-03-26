5aeeda0b15228d47ae0775e786b676a2ef9cdb5b
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-03-26 17:46:36 +0300

    Footer changed

--- not-evil-studios-website/src/components/Footer.js
+++ not-evil-studios-website/src/components/Footer.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import './Footer.css'
 import { Row, Col, Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
-    <Container
-    fluid
-    className='footer-container'
-    >
-        <Row>
-          <Col xl={6} xs={6} style={{textAlign:'center', color:'white'}}>
-            Copyright NOT EVIL STUDIOS
-          </Col>
-          <Col xl={6} xs={6}  style={{textAlign:'center', color:'white'}}>
-            Social media links
-          </Col>
+    <Container fluid className='footer-container'>
+      <Row xs={6} md={6} style={{ color: 'white' }}>
+        <Col className='first-column'>
+          Copyright NOT EVIL STUDIOS
+        </Col>
         </Row>
+      <Row xs={6} md={6}  style={{ color: 'white'  }}>
+        <Col className='socials-column fa-lg'>
+        <a href='https://twitter.com/NotEvilStudios' rel='noreferrer' target="_blank" >
+        <FontAwesomeIcon icon={faTwitter} /> </a>       
+        <a href='https://www.facebook.com/profile.php?id=100089749505346' rel='noreferrer' target="_blank" >
+        <FontAwesomeIcon icon={faFacebook} /> </a>       
+        <a href='https://twitter.com/NotEvilStudios' rel='noreferrer' target="_blank" >
+        <FontAwesomeIcon icon={faYoutube} /> </a>     
+        <a href='https://www.linkedin.com/company/93093211' rel='noreferrer' target="_blank" >
+        <FontAwesomeIcon icon={faLinkedin} /> </a>         
+         </Col>
+      </Row>
     </Container>
   )
 }
