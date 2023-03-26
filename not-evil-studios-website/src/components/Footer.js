2112c2839f20b28d0d8b396a31b0bd68c42d17ea
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-03-26 18:00:32 +0300

    Footer Updated

--- not-evil-studios-website/src/components/Footer.js
+++ not-evil-studios-website/src/components/Footer.js
@@ -7,20 +7,20 @@ import { faFacebook, faTwitter, faLinkedin, faYoutube } from '@fortawesome/free-
 function Footer() {
   return (
     <Container fluid className='footer-container'>
-      <Row xs={6} md={6} style={{ color: 'white' }}>
+      <Row xs={6} md={3} style={{ color: 'white' }} className='row-1'>
         <Col className='first-column'>
           Copyright NOT EVIL STUDIOS
         </Col>
         </Row>
-      <Row xs={6} md={6}  style={{ color: 'white'  }}>
+      <Row xs={6} md={6}  style={{ color: 'white'  }} className='row-2'>
         <Col className='socials-column fa-lg'>
-        <a href='https://twitter.com/NotEvilStudios' rel='noreferrer' target="_blank" >
+        <a href='https://twitter.com/NotEvilStudios' rel='noreferrer' target="_blank"  >
         <FontAwesomeIcon icon={faTwitter} /> </a>       
-        <a href='https://www.facebook.com/profile.php?id=100089749505346' rel='noreferrer' target="_blank" >
+        <a href='https://www.facebook.com/profile.php?id=100089749505346' rel='noreferrer' target="_blank"  >
         <FontAwesomeIcon icon={faFacebook} /> </a>       
-        <a href='https://twitter.com/NotEvilStudios' rel='noreferrer' target="_blank" >
+        <a href='https://twitter.com/NotEvilStudios' rel='noreferrer' target="_blank"  >
         <FontAwesomeIcon icon={faYoutube} /> </a>     
-        <a href='https://www.linkedin.com/company/93093211' rel='noreferrer' target="_blank" >
+        <a href='https://www.linkedin.com/company/93093211' rel='noreferrer' target="_blank"  >
         <FontAwesomeIcon icon={faLinkedin} /> </a>         
          </Col>
       </Row>
