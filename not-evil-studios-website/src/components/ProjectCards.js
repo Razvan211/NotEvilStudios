a9e88b78a0140d77b0e0c1b1b29bb8c466e71b21
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-03-27 20:45:35 +0300

    Tried to solve the netlify error

--- not-evil-studios-website/src/components/ProjectCards.js
+++ not-evil-studios-website/src/components/ProjectCards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './ProjectCards.css'
+import { Link } from 'react-router-dom';
 
 
 export default function ProjectCards(props) {
@@ -11,7 +12,7 @@ export default function ProjectCards(props) {
   <div className="project-card-body">
     <h5 className="project-card-title">{props.title}</h5>
     <p className="project-card-text">{props.description}</p>
-    <a  href={props.link} rel='noreferrer'  className="btn btn-dark ">Go to project</a>
+    <a  as={Link} href={props.link}  rel='noreferrer'  className="btn btn-dark ">Go to project</a>
   </div>
 </div>
   )
