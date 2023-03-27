c90992b949fa1dc2cec9369c89ee44425bc968ef
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-03-27 20:47:37 +0300

    Update ProjectCards.js

--- not-evil-studios-website/src/components/ProjectCards.js
+++ not-evil-studios-website/src/components/ProjectCards.js
@@ -12,7 +12,7 @@ export default function ProjectCards(props) {
   <div className="project-card-body">
     <h5 className="project-card-title">{props.title}</h5>
     <p className="project-card-text">{props.description}</p>
-    <a  as={Link} href={props.link}  rel='noreferrer'  className="btn btn-dark ">Go to project</a>
+    <a  href={props.link}  rel='noreferrer'  className="btn btn-dark ">Go to project</a>
   </div>
 </div>
   )
