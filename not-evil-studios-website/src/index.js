903d3d86a5bef8292d25946e5a82fe2390ba3b8d
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-04-04 18:26:18 +0300

    Google Analytics added

--- not-evil-studios-website/src/index.js
+++ not-evil-studios-website/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
 import App from './App';
-
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-G-VYFQK7C385");
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -12,4 +14,10 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+reportWebVitals(SendAnalytics);
